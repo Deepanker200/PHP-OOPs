62a4ba840ccf67ef39f4dc0456d54077125fd9ca
Deepanker Tiwari <deepamamazing@gmail.com>
2024-08-01 23:36:05 +0530

    added inheritance

--- inheritance.php
+++ inheritance.php
@@ -24,11 +24,25 @@ class employee{
 
 class manager extends employee{
 
+    public $ta=1500;
+    public $phone=200;
+    public $totalSalary;
+
+    function info(){
+        $this->totalSalary=$this->salary + $this->ta + $this->phone;
+
+        echo "<h3>Manager Profile</h3>";
+        echo "Employee Name :" . $this->name."<br>";
+        echo "Employee Age :" . $this->age."<br>";
+        echo "Employee Salary :" . $this->salary."<br>";
+    }
 
 }
 
-$e1=new manager("Rahul",25,2000);
+$e1=new manager("Christ",25,10000);
+$e2=new employee("Rahul",27,2000);
 
 $e1->info();
+$e2->info();
 
 ?>
